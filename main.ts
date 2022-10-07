612f40aa798407b736748d942cc317a99e32333d
junefish <junefish@users.noreply.github.com>
2022-10-06 20:58:26 -0400

    set color to scrolling rainbow

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 let strip = neopixel.create(DigitalPin.P2, 60, NeoPixelMode.RGB)
 basic.forever(function () {
     strip.showRainbow(1, 360)
+    while (true) {
+        strip.rotate(1)
+        strip.show()
+        basic.pause(50)
+    }
 })
