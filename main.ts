ad8ca4eef2a2be5c2896b18872b594ba5a162dc5
junefish <junefish@users.noreply.github.com>
2022-10-06 20:49:15 -0400

    change color to static rainbow

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 let strip = neopixel.create(DigitalPin.P2, 60, NeoPixelMode.RGB)
 basic.forever(function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Violet))
+    strip.showRainbow(1, 360)
 })
