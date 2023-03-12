7e7b08d99e97ac255ee7e681f53ff5f2794a36be
junefish <junefish@users.noreply.github.com>
2023-03-12 14:31:59 -0400

    change brightness

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let strip = neopixel.create(DigitalPin.P2, 60, NeoPixelMode.RGB)
 basic.forever(function () {
-    strip.setBrightness(160)
+    strip.setBrightness(50)
     strip.showRainbow(1, 360)
     while (true) {
         strip.rotate(1)
