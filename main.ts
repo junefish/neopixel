7d97f4829a0d07b03a15b683820041d3e3c48855
junefish <junefish@users.noreply.github.com>
2024-10-04 17:48:10 -0400

    reduce brightness to 30

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let strip = neopixel.create(DigitalPin.P2, 240, NeoPixelMode.RGB)
 basic.forever(function () {
-    strip.setBrightness(40)
+    strip.setBrightness(30)
     strip.showRainbow(1, 360)
     while (true) {
         strip.rotate(1)
